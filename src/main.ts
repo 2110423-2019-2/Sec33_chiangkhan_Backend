635dbb269f59181672e629adc3cd684d88e12b5a
Tanakorn Pisnupoomi <intaniger@gmail.com>
2020-03-19 01:57:11 +0700

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true });
   app.use(cookieParser());
+
+  const options = new DocumentBuilder()
+    .setTitle('Sec33_chiangkhan_Backend example')
+    .setDescription('The Sec33_chiangkhan_Backend API description')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
