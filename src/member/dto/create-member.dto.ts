f11529e8a306478bbf6fca0dbb454e548f48d423
theminer3746 <me@theminerdev.com>
2020-03-19 20:05:51 +0700

    Create create-member dto
    Create DTO to use when registering a new user

---
+++ src/member/dto/create-member.dto.ts
@@ -0,0 +1,96 @@
+import { IsString, IsEnum, IsDateString, Matches, IsOptional, IsArray } from 'class-validator'
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+
+export class CreateMemberDto{
+    
+    @ApiProperty({
+        type: "string",
+        description: "firstname",
+    })
+    @IsString()
+    firstname: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "lastname",
+    })
+    @IsString()
+    lastname: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "username",
+    })
+    @IsString()
+    username: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "password",
+    })
+    @IsString()
+    password: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "email",
+    })
+    @IsString()
+    email: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "phone_num",
+    })
+    @IsString()
+    phone_num: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "bank_account",
+    })
+    @IsString()
+    bank_account: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "bank_account_branch",
+    })
+    @IsString()
+    bank_account_branch: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "credit_card_number",
+    })
+    @IsString()
+    credit_card_number: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "credit_card_expiry",
+    })
+    @IsString()
+    credit_card_expiry: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "credit_card_security",
+    })
+    @IsString()
+    credit_card_security: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "driving_license",
+    })
+    @IsString()
+    driving_license: string;
+
+    @ApiProperty({
+        type: "string",
+        description: "address",
+    })
+    @IsString()
+    address: string;
+}
