bec6217461b0503afa5782f42d63313f42488461
theminer3746 <me@theminerdev.com>
2020-03-19 20:28:04 +0700

    Hash the password with SHA-256 before inserting it into the database

--- src/member/create-member.pipe.ts
+++ src/member/create-member.pipe.ts
@@ -2,18 +2,24 @@ import { Injectable, PipeTransform, ArgumentMetadata, BadRequestException } from
 import { CreateMemberDto } from "./dto/create-member.dto";
 import { validate } from 'class-validator';
 import { plainToClass } from 'class-transformer';
+import { sha256, Hasher } from "js-sha256";
 
 @Injectable()
 export class CreateMemberPipe implements PipeTransform<CreateMemberDto, Promise<CreateMemberDto>> {
-    async transform(value: any, { metatype }: ArgumentMetadata) {
+    async transform(value: CreateMemberDto, { metatype }: ArgumentMetadata) {
         if (!metatype || !this.toValidate(metatype)) {
             return value;
         }
+
         const object = plainToClass(metatype, value);
         const errors = await validate(object);
+
         if (errors.length > 0) {
             throw new BadRequestException('Validation failed');
         }
+
+        value.password = sha256.create().update(value.password).hex()
+
         return value;
     }
 
