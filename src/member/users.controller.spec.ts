b30c413dd3709f731c5b0fea4198a01186416809
theminer3746 <me@theminerdev.com>
2020-03-25 00:11:23 +0700

    Create unit test for UserController

---
+++ src/member/users.controller.spec.ts
@@ -0,0 +1,54 @@
+import { UserController } from "./users.controller"
+import { MemberService } from "./member.service"
+import { Test } from "@nestjs/testing"
+import { CreateMemberDto } from "./dto/create-member.dto"
+
+jest.mock('./member.service')
+
+describe('UserController', () => {
+    let userController: UserController
+    let memberService: MemberService
+
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            providers: [
+                MemberService,
+            ],
+            controllers: [
+                UserController
+            ],
+        }).compile();
+
+        userController = moduleRef.get<UserController>(UserController)
+        memberService = moduleRef.get<MemberService>(MemberService)
+    })
+
+    describe('Register a new user', () => {
+        it('Should call MemberService.registerService', () => {
+            userController.register(new CreateMemberDto())
+            expect(memberService.registerService).toBeCalled()
+        })
+
+        it('Should call MemberService.registerService with correct arguments', () => {
+            let testData = {
+                firstname: "Test",
+                lastname: "User",
+                username: "Lorem",
+                password: "password",
+                email: "me@example.com",
+                phone_num: "12345678",
+                bank_account: "78798564",
+                bank_account_branch: "Bangkok",
+                credit_card_number: "1234567891234567",
+                credit_card_expiry: "11/22",
+                credit_card_security: "777",
+                driving_license: "AB1234",
+                address: "This is a real address"
+            }
+            
+            userController.register(testData)
+
+            expect(memberService.registerService).toBeCalledWith(testData)
+        })
+    })
+})
