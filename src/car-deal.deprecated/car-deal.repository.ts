ca28fb8a3c05aa5735c60bf7c4e2419ebefc8d28
Tanakorn Pisnupoomi <intaniger@gmail.com>
2020-03-29 15:01:43 +0700

    deprecate car-deal

--- src/car-deal.deprecated/car-deal.repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { CarDeal } from "./car-deal.entity";
-
-@EntityRepository(CarDeal)
-export class CarDealRepository extends Repository<CarDeal> {
-
-}
