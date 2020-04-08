6db03bb8d0163e0c75ca5a6e4d44ea8d81f2144c
Tanakorn Pisnupoomi <intaniger@gmail.com>
2020-04-08 21:29:22 +0700

    remove car-agreement

--- src/car-agreement/car-agreement.repository.ts
+++
@@ -1,8 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { CarAgreement } from "./car-agreement.entity";
-
-
-@EntityRepository(CarAgreement)
-export class CarAgreementRepository extends Repository<CarAgreement>{
-    
-}
