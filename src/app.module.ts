7ec2f474f50fa15395e04984ce136800fb96e11b
root <wutipong1234@hotmail.com>
2020-04-10 22:19:19 +0700

    available api in new branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { getTypeORMConfig } from './config/typeorm.config';
 import { CarModule } from './car/car.module';
 import { AuthModule } from './member/auth.module';
 import { CarReservationModule } from './car-reservation/car-reservation.module';
+import { CarAvailableModule } from './car-available/car-available.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { CarReservationModule } from './car-reservation/car-reservation.module';
     }),
     AuthModule,
     CarModule,
-    CarReservationModule
+    CarReservationModule,
+    CarAvailableModule
   ],
   controllers: [AppController],
   providers: [AppService],
